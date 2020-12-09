3c24d852f3be8e8211a093c19911286bb69d94a1
schityala <37279304+schityala@users.noreply.github.com>
2020-12-08 22:40:26 -0600

    changes to cmd_proc

--- cmd_proc.sv
+++ cmd_proc.sv
@@ -19,8 +19,10 @@ parameter FAST_SIM = 0;
 
 UART_wrapper UART(.cmd(cmd), .cmd_rdy(cmd_rdy), .clr_cmd_rdy(cap_cmd), .RX(RX), .clk(clk), .rst_n(rst_n));
 
-always_ff @(posedge clk)begin 
-	if(cap_cmd) begin 
+always_ff @(posedge clk, negedge rst_n)begin 
+	if(!rst_n)
+		cmd_shft_reg[15:0] = 16'h0000;
+	else if(cap_cmd) begin 
 		cmd_shft_reg[15:0] = cmd[15:0];
 	end else if(shft) begin 
 		cmd_shft_reg[15:0] = {2'b0 , cmd_shft_reg[15:2]}; 
@@ -43,9 +45,9 @@ always_ff @(posedge clk, negedge rst_n) begin
 		last_veer_right = cmd_reg[0];
 end 
 
-always_ff @(posedge clk) begin
-	//if(!rst_n)
-		//tmr = 0;
+always_ff @(posedge clk, negedge rst_n) begin
+	if(!rst_n)
+		tmr = 0;
 	if(rst_tmr)
 		tmr = 0;
 	else
@@ -101,14 +103,16 @@ always_comb begin
 				next_state = READY;
 			end
 		end else if(cmd_shft_reg[1:0] == 2'b11) begin 
-			go = 1;
+			go = 0;
 			rst_tmr = 1;
 			next_state = ASSERT_ERR_OPN_LP_1;
 		end else if(|cmd_shft_reg[1:0]) begin
 			go = 1;
+			rst_tmr = 1;
 			next_state = REGULAR_VEER;
 		end else begin 
 			go = 0;
+			rst_tmr = 1;
 			next_state = IDLE;
 		end 
 	end
@@ -138,7 +142,7 @@ always_comb begin
 				err_opn_lp = -16'h1E0;
 				next_state = ASSERT_ERR_OPN_LP_1;
 			end else begin 
-				go = 1;
+				go = 0;
 				rst_tmr = 1;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end 
@@ -148,7 +152,7 @@ always_comb begin
 				err_opn_lp = 16'h1E0;
 				next_state = ASSERT_ERR_OPN_LP_1;
 			end else begin 
-				go = 1;
+				go = 0;
 				rst_tmr = 1;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end 
@@ -163,6 +167,7 @@ always_comb begin
 				err_opn_lp = 16'h380;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end else begin 
+				rst_tmr = 1;
 				next_state = RST_ERR_OPN_LP;
 			end 
 		end else begin 
@@ -171,13 +176,14 @@ always_comb begin
 				err_opn_lp = -16'h380;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end else begin 
+				rst_tmr = 1;
 				next_state = RST_ERR_OPN_LP;
 			end 
 		end
 	end 
 	
 	RST_ERR_OPN_LP : begin
-		go = 1;
+		go = 1; 
 		err_opn_lp = 0;
 		if(line_present) begin
 			nxt_cmd = 1;
