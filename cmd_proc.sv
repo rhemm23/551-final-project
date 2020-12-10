7900bee90ce1196e5865bd1f9b460674ec804995
schityala <37279304+schityala@users.noreply.github.com>
2020-12-09 20:06:45 -0600

    fixed cmd_proc

--- cmd_proc.sv
+++ cmd_proc.sv
@@ -19,7 +19,9 @@ parameter FAST_SIM = 0;
 
 UART_wrapper UART(.cmd(cmd), .cmd_rdy(cmd_rdy), .clr_cmd_rdy(cap_cmd), .RX(RX), .clk(clk), .rst_n(rst_n));
 
-always_ff @(posedge clk)begin 
+always_ff @(posedge clk, negedge rst_n)begin 
+	if(!rst_n)
+		cmd_shft_reg[15:0] = 16'h0000;
 	if(cap_cmd) begin 
 		cmd_shft_reg[15:0] = cmd[15:0];
 	end else if(shft) begin 
@@ -101,7 +103,7 @@ always_comb begin
 				next_state = READY;
 			end
 		end else if(cmd_shft_reg[1:0] == 2'b11) begin 
-			go = 1;
+			go = 0;
 			rst_tmr = 1;
 			next_state = ASSERT_ERR_OPN_LP_1;
 		end else if(|cmd_shft_reg[1:0]) begin
@@ -138,7 +140,7 @@ always_comb begin
 				err_opn_lp = -16'h1E0;
 				next_state = ASSERT_ERR_OPN_LP_1;
 			end else begin 
-				go = 1;
+				go = 0;
 				rst_tmr = 1;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end 
@@ -148,7 +150,7 @@ always_comb begin
 				err_opn_lp = 16'h1E0;
 				next_state = ASSERT_ERR_OPN_LP_1;
 			end else begin 
-				go = 1;
+				go = 0;
 				rst_tmr = 1;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end 
