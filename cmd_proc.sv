efeb4506ea2d5fe6015d5db1acf97ecdc69fb850
schityala <37279304+schityala@users.noreply.github.com>
2020-12-09 20:14:26 -0600

    Revert "fixed cmd_proc"
    This reverts commit 7900bee90ce1196e5865bd1f9b460674ec804995.

--- cmd_proc.sv
+++ cmd_proc.sv
@@ -19,9 +19,7 @@ parameter FAST_SIM = 0;
 
 UART_wrapper UART(.cmd(cmd), .cmd_rdy(cmd_rdy), .clr_cmd_rdy(cap_cmd), .RX(RX), .clk(clk), .rst_n(rst_n));
 
-always_ff @(posedge clk, negedge rst_n)begin 
-	if(!rst_n)
-		cmd_shft_reg[15:0] = 16'h0000;
+always_ff @(posedge clk)begin 
 	if(cap_cmd) begin 
 		cmd_shft_reg[15:0] = cmd[15:0];
 	end else if(shft) begin 
@@ -103,7 +101,7 @@ always_comb begin
 				next_state = READY;
 			end
 		end else if(cmd_shft_reg[1:0] == 2'b11) begin 
-			go = 0;
+			go = 1;
 			rst_tmr = 1;
 			next_state = ASSERT_ERR_OPN_LP_1;
 		end else if(|cmd_shft_reg[1:0]) begin
@@ -140,7 +138,7 @@ always_comb begin
 				err_opn_lp = -16'h1E0;
 				next_state = ASSERT_ERR_OPN_LP_1;
 			end else begin 
-				go = 0;
+				go = 1;
 				rst_tmr = 1;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end 
@@ -150,7 +148,7 @@ always_comb begin
 				err_opn_lp = 16'h1E0;
 				next_state = ASSERT_ERR_OPN_LP_1;
 			end else begin 
-				go = 0;
+				go = 1;
 				rst_tmr = 1;
 				next_state = ASSERT_ERR_OPN_LP_2;
 			end 
